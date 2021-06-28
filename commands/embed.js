9f1429e53e68bbced4334263648b03397603a2a8
EwozZ_YT <82234268+EwozZYT@users.noreply.github.com>
2021-06-28 21:47:10 +0200

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -5,17 +5,13 @@ module.exports = {
         message.channel.send(new Discord.MessageEmbed()
             .setTitle('Règles du serveur !')
             .setTitle('Règle a respecter sous peine de sanction !')
-            .setDescription('[Recherche Google](https://google.fr) **bonjour**')
+            .setDescription('Règle serveur EwozZ_YT : ')
             .setColor('RANDOM')
-            .addField(`
-            ─━━━━━━⊱❉⊰━━━━━━─
-            ↦𝚁é𝚐𝚕𝚎𝚖𝚎𝚗𝚝 𝙶é𝚗é𝚛𝚊𝚕
-            ─━━━━━━⊱❉⊰━━━━━━─
-            ▸ Toute provocation, insultes est passible de sanctions en fonction de la gravité.
-            ▸ Le racisme, sexisme, discrimination ou autre sera sanctionné d'un warn. (voir plus)
-            ▸ Tout caractère choquant ou pornographiques, etc sont interdites. 
-            ▸ Le non respect du staff sera punie d'un warn, le respect des membres est obligatoire aussi.
-            ▸ L' harcèlement ou les menaces sont interdites.`, true)
+            .addField(`Toute , insultes est passible de sanctions.
+            Le racisme, sexisme, discrimination ou autre sera sanctionné d'un warn.
+            Tout caractère choquant ou pornographiques, etc sont interdites. 
+             le respect des membres et du staff est obligatoire aussi.
+             L' harcèlement ou les menaces sont interdites.`, true)
             .setAuthor('Miku | By EwozZ_YT', 'https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg', 'https://google.fr')
             .setImage('https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
             .setThumbnail('https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
