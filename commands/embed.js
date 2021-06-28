4104f11424b3ff6401cf84d5e051a7d9dcd9b117
EwozZ_YT <82234268+EwozZYT@users.noreply.github.com>
2021-06-28 21:48:15 +0200

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -3,19 +3,13 @@ const Discord = require('discord.js')
 module.exports = {
     run: message => {
         message.channel.send(new Discord.MessageEmbed()
-            .setTitle('Règles du serveur !')
-            .setTitle('Règle a respecter sous peine de sanction !')
-            .setDescription('Règle serveur EwozZ_YT : ')
-            .setColor('RANDOM')
-            .addField(`Toute , insultes est passible de sanctions.
-            Le racisme, sexisme, discrimination ou autre sera sanctionné d'un warn.
-            Tout caractère choquant ou pornographiques, etc sont interdites. 
-             le respect des membres et du staff est obligatoire aussi.
-             L' harcèlement ou les menaces sont interdites.`, true)
-            .setAuthor('Miku | By EwozZ_YT', 'https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg', 'https://google.fr')
-            .setImage('https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
-            .setThumbnail('https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
-            .setFooter('Mention || @everyone ||', 'https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
+            .setTitle('Miku Bot')
+            .setDescription(`Dev By EwozZ_YT`)
+            .setColor('#990000')
+            .setAuthor('EwozZ_YT', 'https://cdn.discordapp.com/attachments/842497309194715220/858757418729472040/2Q.png', 'https://google.fr')
+            .setImage('https://cdn.discordapp.com/attachments/842497309194715220/858757418729472040/2Q.png')
+            .setThumbnail('https://cdn.discordapp.com/attachments/842497309194715220/858757418729472040/2Q.png')
+            .setFooter('Miku : By EwozZ_YT', 'https://cdn.discordapp.com/attachments/842497309194715220/858757418729472040/2Q.png')
             .setTimestamp()
             .setURL('https://google.fr'))
     },
