339bfecbc2c3ecdc6b9616e3b221b28138ea703b
EwozZ_YT <82234268+EwozZYT@users.noreply.github.com>
2021-06-28 21:39:47 +0200

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -7,12 +7,35 @@ module.exports = {
             .setTitle('Règle a respecter sous peine de sanction !')
             .setDescription('[Recherche Google](https://google.fr) **bonjour**')
             .setColor('RANDOM')
-            .addField('Champ 1 **bonjour**', 'Bonjour c\'est moi **bonjour**', true)
-            .addField('Champ 2', 'Blabla', true)
-            .setAuthor('Miku | By EwozZ_YT', 'https://cdn.discordapp.com/attachments/718476721418141728/719563110154764298/logo.png', 'https://google.fr')
-            .setImage('https://cdn.discordapp.com/attachments/718476721418141728/719563122960236644/thumb.png')
-            .setThumbnail('https://cdn.discordapp.com/attachments/718476721418141728/719563110154764298/logo.png')
-            .setFooter('Mention || @everyone ||', 'https://cdn.discordapp.com/attachments/718476721418141728/719563110154764298/logo.png')
+            .addField(`
+            ─━━━━━━⊱❉⊰━━━━━━─
+            ↦𝚁é𝚐𝚕𝚎𝚖𝚎𝚗𝚝 𝙶é𝚗é𝚛𝚊𝚕
+            ─━━━━━━⊱❉⊰━━━━━━─
+            
+            ▸ Toute provocation, insultes est passible de sanctions en fonction de la gravité.
+            ▸ Le racisme, sexisme, discrimination ou autre sera sanctionné d'un warn. (voir plus)
+            ▸ Tout caractère choquant ou pornographiques, etc sont interdites. 
+            ▸ Le non respect du staff sera punie d'un warn, le respect des membres est obligatoire aussi.
+            ▸ L' harcèlement ou les menaces sont interdites.
+            
+              ' Deuxièmement nous voulons des salons propre et lisible donc :
+            
+            ▸ Le spam est interdit.
+            ▸ En cas de soucis entre deux personnes aller en message privé ou nous le reporté 
+            ▸ Le flood est interdit
+             -> voici pour les interdiction passible par un mute. 
+            
+              ' Troisièmement les messages privé ¦ channel vocaux
+            
+            ▸ La pub en message privé est interdite.
+            ▸ Menaces ou intimidations interdites.
+            ▸ Merci de ne pas crié, insulter dans les vocaux.
+            -> voici les interdictions ou nous allons voir pour la sanction, selon la gravité.
+            `, true)
+            .setAuthor('Miku | By EwozZ_YT', 'https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg', 'https://google.fr')
+            .setImage('https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
+            .setThumbnail('https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
+            .setFooter('Mention || @everyone ||', 'https://cdn.discordapp.com/attachments/858745744030957609/859155711677562940/OIP_1.jpg')
             .setTimestamp()
             .setURL('https://google.fr'))
     },
